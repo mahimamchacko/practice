326eed8412b2971a681bb2f4f2c37568e56c62b5
Mahima Chacko <mahimamchacko@gmail.com>
2023-08-21 21:19:23 -0400

    Complete Leetcode 724

---
+++ leetcode/leetcode 724.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/find-pivot-index/
+
+from typing import List
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left = 0
+        right = sum(nums)
+
+        for i in range(len(nums)):
+            right -= nums[i]
+
+            if left == right:
+                return i
+            
+            left += nums[i]
+        
+        return -1
