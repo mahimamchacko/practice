bbbc15ad2331adebf17e0ec06ee90d4e467547e6
Mahima Chacko <mahimamchacko@gmail.com>
2023-08-21 22:49:29 -0400

    Complete Leetcode 700

---
+++ leetcode/leetcode 700.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+from typing import List, Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        while root is not None:
+            if root.val == val:
+                return root
+            if val < root.val:
+                root = root.left
+            else:
+                root = root.right
+        return None
